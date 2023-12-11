da3b3ee34b1c5d1bccb49efa7706c84e86f69021
tutel <john.g.zioupos@gmail.com>
2023-12-11 21:33:52 +0200

    Caps Lock Support
    Made it so that if you have caps lock enable it will still detect keystrokes

--- newmain.js
+++ newmain.js
@@ -55,7 +55,9 @@ function beginTest() {
     timediffs.length = 0;
     beginTime = -1;
     key1 = $('#key1').val();
+	key1upper = key1.toUpperCase();
     key2 = $('#key2').val();
+	key2upper = key2.toUpperCase();
     mouse = $("input[name='cmouse']").prop("checked");
     $("div#status").html("Test ready, press key 1 or key 2 to begin.");
     $("div#Result").html("\
@@ -66,7 +68,9 @@ function beginTest() {
     localStorage.setItem('clickLimit', clickLimit);
     localStorage.setItem('timeLimit', timeLimit);
     localStorage.setItem('key1', key1);
+	localStorage.setItem('key1upper', key1upper);
     localStorage.setItem('key2', key2);
+	localStorage.setItem('key2upper', key2upper);
     localStorage.setItem('mouse', mouse);
     std = 0;
     $("button#submit").hide();
@@ -178,7 +182,7 @@ $(document).keypress(function(event)
     {
         //if (String.fromCharCode(event.which) == key1 || String.fromCharCode(event.which) == key2)
         //{
-            if ((String.fromCharCode(event.which) == key1) || (String.fromCharCode(event.which) == key2)) // Any reason there are two of these? Removed one...
+            if ((String.fromCharCode(event.which) == key1) || (String.fromCharCode(event.which) == key1upper)||(String.fromCharCode(event.which) == key2) || (String.fromCharCode(event.which) == key2upper)) // Any reason there are two of these? Removed one...
             {
                 switch (beginTime)
                 {
@@ -268,10 +272,18 @@ $(document).ready(function() {
         $("input#key1").val("z");
     else
         $("input#key1").val(localStorage.getItem('key1'));
+	if(!localStorage.getItem('key1upper'))
+		$("input#key1upper").val("Z");
+	else
+		$("input#key1upper").val(localStorage.getItem('key1upper'));
     if(!localStorage.getItem('key2'))
         $("input#key2").val("x");
     else
         $("input#key2").val(localStorage.getItem('key2'));
+	if(!localStorage.getItem('key2upper'))
+		$("input#key2upper").val("X");
+	else
+		$("input#key2upper").val(localStorage.getItem('key2upper'));
     if(!localStorage.getItem('timeLimit'))
         $("input#clickTime").val("10");
     else
